a4fc301dd6a94397b13740789c33af2485311fb9
Mohit Suthar <mohitsuthar266@gmail.com>
2026-03-17 09:56:37 +0530

    Add files via upload

---
+++ LargestElement.java
@@ -0,0 +1,19 @@
+package com.dsaAtoZ.Array;
+
+public class LargestElement {
+    public static void main(String[] args) {
+        int[] arr = {4, 4, 6, 2, 5, 7, 9, 1, 3};
+        System.out.println("Largest Number is : " +getLargestElement(arr, arr.length));
+    }
+
+    public static int getLargestElement(int[] arr, int n){
+        int largestNum = arr[0];
+
+        for (int i = 1; i < n; i++){
+            if (arr[i] > largestNum){
+                largestNum = arr[i];
+            }
+        }
+        return largestNum;
+    }
+}
