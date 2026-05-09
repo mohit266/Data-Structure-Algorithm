d808cb804d3e68031d2878e50746a730c68461ee
Mohit Suthar <mohitsuthar266@gmail.com>
2026-05-09 12:36:12 +0530

    DSA

--- SlidingWindowAndTwoPointer/FruitIntoBaskets.java
+++ SlidingWindowAndTwoPointer/FruitIntoBaskets.java
@@ -39,4 +39,37 @@ public class FruitIntoBaskets {
         }
         return maxLen;
     }
+
+    public int totalFruitsOptimalOne(int[] fruits) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        int left = 0;
+        int right = 0;
+
+        int n = fruits.length;
+        int maxLen = 0;
+
+        while (right < n){
+
+            mp.put(fruits[right],
+                    mp.getOrDefault(fruits[right], 0) + 1);
+
+            if (mp.size() > 2) {
+
+                mp.put(fruits[left], mp.get(fruits[left]) - 1);
+
+                if (mp.get(fruits[left]) == 0) {
+                    mp.remove(fruits[left]);
+                }
+
+                left++;
+            }
+
+            if (mp.size() <= 2){
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+            right++;
+        }
+        return maxLen;
+    }
 }
