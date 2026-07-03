1290d0bd94f810f85a8d47c5a673ef3f127aea93
Mohit Suthar <mohitsuthar266@gmail.com>
2026-07-03 22:23:19 +0530

    DSA

--- Graphs/DisjointSetMain.java
+++ Graphs/DisjointSetMain.java
@@ -4,18 +4,18 @@ public class DisjointSetMain {
     public static void main(String[] args) {
 
         DisjointSet ds = new DisjointSet(7);
-        ds.unionByRank(1, 2);
-        ds.unionByRank(2, 3);
-        ds.unionByRank(4, 5);
-        ds.unionByRank(6, 7);
-        ds.unionByRank(5, 6);
+        ds.unionBySize(1, 2);
+        ds.unionBySize(2, 3);
+        ds.unionBySize(4, 5);
+        ds.unionBySize(6, 7);
+        ds.unionBySize(5, 6);
 
         if (ds.find(3, 7))
             System.out.println("They belong to the same components.");
         else
             System.out.println("They do not belong to the same components.");
 
-        ds.unionByRank(3, 7);
+        ds.unionBySize(3, 7);
 
         if (ds.find(3, 7))
             System.out.println("They belong to the same components.");
