e76eb4850037177878c509fcba3eba0003e583bd
Mohit Suthar <mohitsuthar266@gmail.com>
2026-08-01 21:41:54 +0530

    DSA

---
+++ DailyChallenges/PredictWinner.java
@@ -0,0 +1,36 @@
+package com.dsa.DailyChallenges;
+
+public class PredictWinner {
+
+    public static void main(String[] args) {
+
+        PredictWinner p = new PredictWinner();
+
+        int[] nums = {1,5,233, 7};
+        System.out.println(p.predictTheWinner(nums));
+    }
+
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        Integer[][] dp = new Integer[n][n];
+
+        return memo(nums, 0, n - 1, dp) >= 0;
+    }
+
+    private int memo(int[] nums, int left, int right, Integer[][] dp) {
+
+        if (left == right) {
+            return nums[left];
+        }
+
+        if (dp[left][right] != null) {
+            return dp[left][right];
+        }
+
+        int pickLeft = nums[left] - memo(nums, left + 1, right, dp);
+        int pickRight = nums[right] - memo(nums, left, right - 1, dp);
+
+        return dp[left][right] = Math.max(pickLeft, pickRight);
+    }
+
+}
