c9f17034b095695cae3c42a787abb54f6bb43d7a
Mohit Suthar <mohitsuthar266@gmail.com>
2026-09-17 09:58:44 +0530

    DSA

--- PatternWise/Tree/BinaryTree/Diameter.java
+++ PatternWise/Tree/BinaryTree/Diameter.java
@@ -6,7 +6,34 @@ import com.dsa.BinaryTree.TreeNode;
 public class Diameter {
 
     public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
 
+
+        Diameter d = new Diameter();
+
+        System.out.println(d.diameterOfBinaryT(root));
+    }
+
+    public int diameterOfBinaryT(TreeNode root) {
+        int[] dia = new int[1];
+        height(root, dia);
+        return dia[0];
+    }
+
+    public int height(TreeNode node, int[] dia){
+        if (node == null){
+            return 0;
+        }
+        int left = height(node.left, dia);
+        int right = height(node.right, dia);
+
+        dia[0] = Math.max(left + right, dia[0]);
+
+        return 1 + Math.max(left, right);
     }
 
     public int diameterOfBinaryTree(TreeNode root) {
