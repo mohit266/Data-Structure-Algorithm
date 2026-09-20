fed60bd9e696a24f1eb1b35506afb3715fe5f51b
Mohit Suthar <mohitsuthar266@gmail.com>
2026-09-20 10:56:08 +0530

    DSA

---
+++ DailyChallenges/ReverseDegreeOfAString.java
@@ -0,0 +1,25 @@
+package com.dsa.DailyChallenges;
+
+// 3498. Reverse Degree of a String
+public class ReverseDegreeOfAString {
+
+    public static void main(String[] args) {
+        ReverseDegreeOfAString rev = new ReverseDegreeOfAString();
+
+        String s = "zaza";
+        System.out.println(rev.reverseDegree(s));
+    }
+
+    public int reverseDegree(String s) {
+
+        int degree = 0;
+
+        for (int i = 0; i < s.length(); i++){
+            int sum = (122 - s.charAt(i) + 1) * (i + 1);
+            degree += sum;
+        }
+
+        return degree;
+    }
+
+}
